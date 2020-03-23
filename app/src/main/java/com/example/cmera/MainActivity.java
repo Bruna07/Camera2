6bf7101b9487cfddaedf3cb92959e532553af29a
ygonb <ygo@pesqueira.ifpe.edu.br>
2020-03-23 17:47:11 -0300

    Testes com Handler e AsyncTask

--- app/src/main/java/com/example/cmera/MainActivity.java
+++ app/src/main/java/com/example/cmera/MainActivity.java
@@ -13,9 +13,12 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -25,16 +28,19 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imagem;
-    private static final int IMAGE_VIEW_ACTIVITY_REQUEST_CODE   = 101;//para abrir a galeria
+    private static final int IMAGE_VIEW_ACTIVITY_REQUEST_CODE = 101;//para abrir a galeria
+    private Bitmap bitmap;
+    private static final String URL = "https://livroandroid.com.br/imgs/livro_android.png";
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         imagem = (ImageView) findViewById(R.id.imagem1);
+        imagem = (ImageView) findViewById(R.id.imagem1);
 
-         final ImageButton galeria = (ImageButton) findViewById(R.id.btgaleria);
+        final ImageButton galeria = (ImageButton) findViewById(R.id.btgaleria);
 
         ImageButton camera = (ImageButton) findViewById(R.id.btcamera);
 
@@ -45,9 +51,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v2) {
 
                 //Intent intent2 = new Intent( Intent.ACTION_VIEW );
-              //  startActivityForResult( intent2, IMAGE_VIEW_ACTIVITY_REQUEST_CODE );
+                //  startActivityForResult( intent2, IMAGE_VIEW_ACTIVITY_REQUEST_CODE );
                 Intent intentPegaFoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                startActivityForResult(intentPegaFoto,IMAGE_VIEW_ACTIVITY_REQUEST_CODE);
+                startActivityForResult(intentPegaFoto, IMAGE_VIEW_ACTIVITY_REQUEST_CODE);
 
             }
         });
@@ -65,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
 //*=*=*=*=*=*=*=*=*=*=*=*=*=*=*= PARTE PARA CAMERA *=*=*=*=**=*=*=*=*=*=*=*=*=*=*=*=*=*=**=*=*=*=*=
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},0);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
             // VERIFICA SE O APP POSSUI A PERMISSÃO DE ACESSO A CÂMERA
         }
 
@@ -75,23 +81,80 @@ public class MainActivity extends AppCompatActivity {
                 tirarFoto();
             }
         });
+
+        downloadImagem();
+        repeteAcao();
+
+
     }
 
-    public void tirarFoto(){
-        Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent,1);
+    public void tirarFoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
     }
+
     @Override
     //*=*=*=*=*=*=*Retorna o resultado da intent*=*=*=*=*=*=*=*=*
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode==1 && resultCode == RESULT_OK){
+        if (requestCode == 1 && resultCode == RESULT_OK) {
             //CONVERTE RESULTADOS
             Bundle extras = data.getExtras();
-            Bitmap formatImagem= (Bitmap) extras.get("data");
+            Bitmap formatImagem = (Bitmap) extras.get("data");
             imagem.setImageBitmap(formatImagem);
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+
     }
+
+    // TESTES COM HANDLER E REPETIÇÕES
+    private void repeteAcao (){
+        new Thread(){
+            @Override
+            public void run(){
+                Log.i("Ygo","Repete");
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        repeteAcao();
+                    }
+                },3000);
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+
+
+    // TESTES COM ASYNCTASK
+    private void downloadImagem (){
+        DownloadTask tarefa = new DownloadTask();
+        tarefa.execute();
+    }
+
+    private class DownloadTask extends AsyncTask<Void, Void, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.i("Ygo","onPreExecute");
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            Log.i("Ygo","doInBackground");
+            bitmap = null;
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap==null){
+            Log.i("Ygo","onPostExecute");}
+        }
+    }
+
 
 }
